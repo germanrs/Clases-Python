759b40c557cbaca59fa074a47411b0ac209a0b6f
German Ruiz <gars1029@gmail.com>
2011-11-21 10:02:16 -0600

    Agregando la tarea 3

---
+++ Tarea3/cuadrados.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Entrar el limite: 100
+# Los números cuadrados son:
+# 1
+# 4
+# 9
+# 16
+# 25
+# 36
+# 49
+# 64
+# 81
+# 100
+
+limite = int(raw_input('Ingrese el limite: '))
+inicio = 1
+resultado = inicio * inicio
+print 'Los números cuadrados son: '
+
+while resultado <= limite:
+    print resultado
+    inicio = inicio + 1
+    resultado = inicio * inicio
+
