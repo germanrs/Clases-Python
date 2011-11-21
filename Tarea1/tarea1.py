baac367deae7a43aac4872d6341e7f1549e9bfa7
German Ruiz <gars1029@gmail.com>
2011-11-21 10:00:55 -0600

    Agregando la tarea 1

---
+++ Tarea1/tarea1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Como te llamas? Daniel
+# Hola Daniel, cuantos anhos tienes? 25
+# En tu proximo cumpleaños tendrás 26 años
+nombre = raw_input('Como te llamas?: ')
+try:
+    edad = int(raw_input('Hola, %s, cuantos años tienes: ' % (nombre)))
+    print 'En tu proximo cumpleaños tendras: %d años ' % (edad+1)
+except ValueError:
+    print 'Ese no es una edad, adios...'
