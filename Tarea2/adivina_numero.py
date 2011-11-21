e54c3d75fbc48318b4176b8971fd792f249e1e43
German Ruiz <gars1029@gmail.com>
2011-11-21 10:01:42 -0600

    Agregando la tarea 2

---
+++ Tarea2/adivina_numero.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Entrar el limite: 100
+# Los números cuadrados son:
+# 1
+# 4
+# 9
+# 16
+# 25
+# 36
+# 49
+# 64
+# 81
+# 100
+#
+import random
+
+numero_aleatorio = random.randint(1,10)
+intentos = 0
+
+entrada_numero = None
+
+while entrada_numero != numero_aleatorio:
+    entrada_numero = int(raw_input('Adivina el numero! '))
+    intentos = intentos + 1
+    if entrada_numero > numero_aleatorio:
+        print 'Muy alto'
+    else:
+        print 'Muy bajo'
+print 'Adivinaste el numero en %d intentos' % (intentos)
